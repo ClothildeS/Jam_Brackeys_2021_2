f46ce7863c82517f6db3682ab7980c82c27d9c28
ClothildeS <clothilde.schulz@hotmail.com>
2021-08-27 20:40:51 +0200

    Monster AI

--- JAM_Brackeys_2021_2/Assets/Scripts/MonsterBehaviour.cs
+++ JAM_Brackeys_2021_2/Assets/Scripts/MonsterBehaviour.cs
@@ -1,13 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class MonsterBehaviour : MonoBehaviour
 {
 
-    // For now the movement is constant to the right, must be via nav mesh agent later
-    private void Update()
+    public Transform monster;
+    public Transform target;
+    public float speed = 20f;
+
+    public float nextWayPointDistance = 2f;
+    Path path;
+    int currentWayPoint = 0;
+    bool reachEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    private void Start()
     {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        target = FindClosestTarget().transform;
+
+
+        InvokeRepeating("UpdatePath", 0f, 1f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            if (target != null)
+            {
+                seeker.StartPath(rb.position, target.position, OnPathComplete);
+            }
+
+        }
+
+    }
+
+    // For now the movement is constant to the right, must be via nav mesh agent later
+    private void FixedUpdate()
+    {
+
+        if (GameObject.FindGameObjectsWithTag("MonsterDestroy") == null)
+        {
+            return;
+        }
+        target = FindClosestTarget().transform;
+
+
+
+        if (path == null)
+        {
+            return;
+        }
+
+        if (currentWayPoint >= path.vectorPath.Count)
+        {
+            reachEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
+
+        if (distance < nextWayPointDistance)
+        {
+            currentWayPoint++;
+        }
+
+        if (force.x >= 0.02f)
+        {
+            monster.localScale = new Vector3(-0.5f, 0.5f, 1f);
+        }
+        else if (force.x < -0.02f)
+        {
+            monster.localScale = new Vector3(0.5f, 0.5f, 1f);
+        }
+
+        // Update animations
+        if (Mathf.Abs(rb.velocity.x) >= 0.02f)
+        {
+            GetComponentInChildren<Animator>().SetBool("Running", true);
+        }
+        else if (Mathf.Abs(rb.velocity.x) < 0.02f)
+        {
+            GetComponentInChildren<Animator>().SetBool("Running", false);
+        }
 
     }
 
@@ -30,5 +121,35 @@ public class MonsterBehaviour : MonoBehaviour
         }
 
 
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
+
+    public GameObject FindClosestTarget()
+    {
+
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("MonsterDestroy");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
     }
 }
